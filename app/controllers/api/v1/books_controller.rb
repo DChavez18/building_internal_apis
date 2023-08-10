89efe7636d976c39af0a8951dfb6a189ee00e193
Derek Chavez <frrenose17@yahoo.com>
2023-08-10 15:53:39 -0600

    feat: add create action in api controller

--- app/controllers/api/v1/books_controller.rb
+++ app/controllers/api/v1/books_controller.rb
@@ -1,4 +1,18 @@
 class Api::V1::BooksController < ApplicationController
   def index
+    render json: Book.all
   end
+
+  def show
+    render json: Book.find(params[:id])
+  end
+
+  def create
+    render json: Book.create(book_params)
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author, :summary, :genre, :number_sold)
+    end
 end
--- spec/requests/api/v1/books_request_spec.rb
+++ spec/requests/api/v1/books_request_spec.rb
@@ -6,6 +6,82 @@ describe "Books API" do
 
     get '/api/v1/books'
 
-    expect(response).to be_successfull
+    expect(response).to be_successful
+    
+    books = JSON.parse(response.body)
+
+    expect(books.count).to eq(3)
+
+    books.each do |book|
+      expect(book).to have_key("id")
+      expect(book["id"]).to be_an(Integer)
+
+      expect(book).to have_key("title")
+      expect(book["title"]).to be_a(String)
+
+      expect(book).to have_key("author")
+      expect(book["author"]).to be_a(String)
+
+      expect(book).to have_key("genre")
+      expect(book["genre"]).to be_a(String)
+
+      expect(book).to have_key("summary")
+      expect(book["summary"]).to be_a(String)
+
+      expect(book).to have_key("number_sold")
+      expect(book["number_sold"]).to be_an(Integer)
+    end
   end
+
+  it "can get one book bu its id" do
+    id = create(:book).id
+
+    get "/api/v1/books/#{id}"
+
+    book = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+
+    expect(book).to have_key(:id)
+    expect(book[:id]).to eq(id)
+
+    expect(book).to have_key(:title)
+    expect(book[:title]).to be_a(String)
+  
+    expect(book).to have_key(:author)
+    expect(book[:author]).to be_a(String)
+  
+    expect(book).to have_key(:genre)
+    expect(book[:genre]).to be_a(String)
+  
+    expect(book).to have_key(:summary)
+    expect(book[:summary]).to be_a(String)
+  
+    expect(book).to have_key(:number_sold)
+    expect(book[:number_sold]).to be_an(Integer)
+  end
+
+  it "can create a new book" do
+    book_params = ({
+                    title: "Murder",
+                    author: "Agatha Christie",
+                    genre: "mystery",
+                    summary: "Good",
+                    number_sold: 2347
+    })
+
+    headers = {"CONTENT_TYPE" => "application/json"}
+
+    post "/api/v1/books", headers: headers, params: JSON.generate(book: book_params)
+    created_book = Book.last
+
+    expect(response).to be_successful
+    expect(created_book.title).to eq(book_params[:title])
+    expect(created_book.author).to eq(book_params[:author])
+    expect(created_book.summary).to eq(book_params[:summary])
+    expect(created_book.genre).to eq(book_params[:genre])
+    expect(created_book.number_sold).to eq(book_params[:number_sold])
+  end
+
+  it "c"
 end
